5832e858522679e033f2dda30ab3a1a9776f63b8
lysandroJulio <lysandro_julio@hotmail.com>
2019-05-10 20:48:18 -0300

    inclusao da tela inicial do sistema

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('principal')
+
+@section('tituloPagina')
+	LA Services
+@stop
+
+@section('corpo')
+	<div class="">
+		<br><br>
+		<h1 align="center">Bem vindo ao Sistema de Cadastro de Funcionarios e Avaliação de Desempenho da LA Services</h1>
+	</div>
+@stop
